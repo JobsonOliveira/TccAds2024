99164c5516281d603f65d7801d9f934129316b27
carlosefl <106102170+carlosefl@users.noreply.github.com>
2024-04-14 14:23:49 -0300

    estrutura do arquivo

--- inter/urls.py
+++ inter/urls.py
@@ -22,6 +22,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('login.urls')),
+    path('cadastro/',include('cadastro.urls')),
+    path('home/', include('principal.urls'))
 ]
 
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
